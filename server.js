bcab06323d53e650c3152e219bcc12a403c20d59
estherkimyunjung <estherkimyunjung@gmail.com>
2021-04-28 03:37:19 -0500

    add socket io for room

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -15,4 +16,9 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+io.on("connection", (socket) => {
+  socket.on("join-room", () => {
+    console.log("joined room");
+  });
+});
 server.listen(3030);
